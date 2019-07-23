cc50f3af6cd179ae6deca5cb2edc894b4ce629c6
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2019-07-23 16:11:56 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset = "UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GCC | Retiro Salvador</title>
+    <meta name="description" content="Coral da Igreja Evangélica Assembleia de Deus em Retiro, Salvador. Para todos os membros. Maestro Alcides Augusto. Participe!">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">  
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>    
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>    
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</head>
+<body class="container-fluid text-center">
+
+	<h1>Grande Coral da Congregação</h1>
+	<h2>Assembléia de Deus em Retiro - Salvador(BA)</h2>
+	
+	<h3>Portal de comunicação!</h3>
+	<p>Aqui o coralista encontra as informações mais relevantes!</p>
+	
+	<hr>
+	
+	<section>
+		<h2>Inscrições Abertas!</h2>
+		<p>Leia os termos do projeto e faça sua inscrição: </p>
+		<ul style="list-style: none; margin-top: 30px;">			
+			<li style="margin-bottom: 20px;"><a href="https://docs.google.com/document/d/19pKcExYHhwoSsVfotw9ePmKxnJVHkrBAtuGOLM2i_Ws/edit?usp=sharing" target="_blank">Documentação Grande Coral</a></li>
+			<li style="margin-bottom: 20px;"><a href="https://www.youtube.com/watch?v=nB5obrZ6hBk&list=PLyh_oxs4z0xvd_UMK5BXUhMe1rNiPN_00" target="_blank">Assista as instruções!</a></li>
+			<li><button type="button" class="btn btn-primary btn-md" onclick=irInscreverse()>Inscreva-se agora!</button></li>
+		</ul>
+
+			
+		
+
+		
+	</section>
+
+</body>
+<script type="text/javascript">
+	function irInscreverse(){
+		window.open("https://forms.gle/kzo7YXtfG5QedGM67");
+	}
+</script>
+</html>
