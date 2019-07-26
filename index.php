7d4e1c9e33d68e8dbd46fc61b19e88d48e4a0ab4
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2019-07-26 01:42:01 -0300

    Correct semantic tags and good stylization

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset = "UTF-8">
+    <meta charset = "utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>GCC | Retiro Salvador</title>
@@ -14,33 +14,25 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>    
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
 </head>
-<body class="container-fluid text-center">
-
-	<h1>Grande Coral da Congregação</h1>
-	<h2>Assembléia de Deus em Retiro - Salvador(BA)</h2>
-	
-	<h3>Portal de comunicação!</h3>
-	<p>Aqui o coralista encontra as informações mais relevantes!</p>
-	
-	<hr>
-	
-	<section>
+<body class="container-fluid text-center p-0">	
+	<header class="text-white py-3" style="background-color: darkblue">
+		<h1 id="siteTitle">Grande Coral da Congregação</h1>
+		<span id="subtitle">Assembléia de Deus em Retiro - Salvador(BA)</span>
+	</header>
+	<article class="text-warning py-3" style="background-color: darkblue">
+		<h2 class="">Portal de comunicação</h2>
+		<p class="">Aqui o coralista encontra as informações mais relevantes!</p>
+	</article>
+	<article class="py-3">
 		<h2>Inscrições Abertas!</h2>
 		<p>Leia os termos do projeto e faça sua inscrição: </p>
 		<ul style="list-style: none; margin-top: 30px;">			
 			<li style="margin-bottom: 20px;"><a href="https://docs.google.com/document/d/19pKcExYHhwoSsVfotw9ePmKxnJVHkrBAtuGOLM2i_Ws/edit?usp=sharing" target="_blank">Documentação Grande Coral</a></li>
 			<li style="margin-bottom: 20px;"><a href="https://www.youtube.com/watch?v=nB5obrZ6hBk&list=PLyh_oxs4z0xvd_UMK5BXUhMe1rNiPN_00" target="_blank">Assista as instruções!</a></li>
-			<li><button type="button" class="btn btn-primary btn-md" onclick=irInscreverse()>Inscreva-se agora!</button></li>
-		</ul>
-
-			
-		
-
-		
-	</section>
-
+			<li><button type="button" class="btn btn-warning" onclick=irInscreverse()>Inscreva-se agora!</button></li>
+		</ul>		
+	</article>
 
 <script>
 	function irInscreverse(){
