4ef14175f3ecdd3618bc56935753f9845cf0aaec
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2019-08-27 02:17:58 -0300

    Calendário

--- index.php
+++ index.php
@@ -20,11 +20,11 @@
 		<h1 id="siteTitle">Grande Coral da Congregação</h1>
 		<span id="subtitle">Assembléia de Deus em Retiro - Salvador(BA)</span>
 	</header>
-	<article class="text-warning py-3" style="background-color: darkblue">
+	<section class="text-warning py-3" style="background-color: darkblue">
 		<h2 class="">Portal de comunicação</h2>
 		<p class="">Aqui o coralista encontra as informações mais relevantes!</p>
-	</article>
-	<article class="py-3">
+	</section>
+	<section class="py-3">
 		<h2>Inscrições Abertas!</h2>
 		<p>Leia os termos do projeto e faça sua inscrição: </p>
 		<ul class="mt-5 pl-0" style="list-style: none;">			
@@ -32,7 +32,17 @@
 			<li style="margin-bottom: 20px;"><a href="https://www.youtube.com/watch?v=nB5obrZ6hBk&list=PLyh_oxs4z0xvd_UMK5BXUhMe1rNiPN_00" target="_blank">Assista as instruções!</a></li>
 			<li><button type="button" class="btn btn-warning" onclick=irInscreverse()>Inscreva-se agora!</button></li>
 		</ul>		
-	</article>
+	</section>
+	<section class="py-3" style="background-color: #fb9b38">
+		<h2>Calendário do Grande Coral</h2>
+		<h3>Acompanhe os eventos!</h3>
+		<iframe src="https://calendar.google.com/calendar/b/1/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FFortaleza&amp;src=Z3JhbmRlY29yYWxyZXRpcm9zc2FAZ21haWwuY29t&amp;color=%23039BE5&amp;showTitle=0&amp;mode=AGENDA&amp;showNav=1&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=0&amp;showTz=0" 
+			class = "border border-0 rounded-sm m-4 shadow"
+			style="overflow: hidden;"
+			width="80%" 
+			height="400">
+		</iframe>
+	</section>
 
 <script>
 	function irInscreverse(){
